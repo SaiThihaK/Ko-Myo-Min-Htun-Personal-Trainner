3081df11627111a7817dd032dda91cd72b46b7ae
Sai <saithihak2@gmail.com>
2024-04-28 05:21:26 +0700

    mobile toggle button

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -57,159 +57,17 @@ const Navbar: React.FC<Props> = (props) => {
       "start"
     );
 
-    const menuTimeline = gsap.timeline({ paused: true });
-
-    menuTimeline.to(fullPageMenuRef.current, {
-      duration: 0,
-      display: "block",
-      ease: "Expo.easeInOut",
-    });
-    menuTimeline.from(".menu-bg span", {
-      duration: 1,
-      x: "100%",
-      stagger: 0.1,
-      ease: "Expo.easeInOut",
-    });
-    menuTimeline.from(
-      navLinkRef.current,
-      {
-        duration: 1.5,
-        y: "100%",
-        stagger: 0.2,
-        ease: "Expo.easeInOut",
-      },
-      "-=0.5"
-    );
-    menuTimeline.from(
-      ".social-links li",
-      {
-        duration: 1,
-        y: "-100%",
-        opacity: 0,
-        stagger: 0.1,
-        ease: "Expo.easeInOut",
-      },
-      "-=0.5"
-    );
-    menuTimeline.kill();
     menuBar.reverse();
     const toggleButton = menuToggleRef.current;
     toggleButton.addEventListener("click", () => {
       menuBar.reversed(!menuBar.reversed());
       setToggle(!toggle);
-      menuTimeline.reversed(!menuTimeline.reversed());
     });
 
     return () => {
       menuBar.kill();
-      menuTimeline.kill();
     };
   }, []);
-
-  if (toggle)
-    return (
-      <>
-        <style jsx>
-          {`
-            .fullpage-menu {
-              position: fixed;
-              left: 0;
-              top: 0;
-              width: 100%;
-              height: 100vh;
-              display: none;
-            }
-            .fullpage-menu-inner {
-              display: flex;
-              align-items: center;
-              height: 100%;
-              padding: 100px 60px;
-            }
-            .menu-bg {
-              height: 100%;
-              width: 100%;
-              position: absolute;
-              left: 0;
-              top: 0;
-            }
-            .menu-bg span {
-              height: 33.334%;
-              width: 100%;
-              background: #e50914;
-              display: block;
-            }
-
-            nav {
-              position: relative;
-              z-index: 10;
-            }
-
-            nav ul {
-              list-style-type: none;
-            }
-            nav li {
-              overflow: hidden;
-              transition: transform 300ms ease-in-out 0s;
-            }
-            nav li + li {
-              margin-top: 20px;
-            }
-            nav li a {
-              font-size: 100px;
-              color: #fff;
-              text-decoration: none;
-              text-transform: uppercase;
-              font-weight: 400;
-              display: inline-block;
-              line-height: 1;
-            }
-            nav li:hover {
-              transform: translateX(50px);
-            }
-          `}
-        </style>
-        <div
-          ref={fullPageMenuRef}
-          className="fixed top-0 left-0 w-full h-screen"
-        >
-          <div className="flex items-center justify-center h-full px-24 py-12">
-            <div className="absolute inset-0">
-              <div className="menu-bg absolute inset-0">
-                <span className="block h-1/3 w-full bg-red-600"></span>
-                <span className="block h-1/3 w-full bg-red-600"></span>
-                <span className="block h-1/3 w-full bg-red-600"></span>
-              </div>
-            </div>
-
-            <nav className="relative">
-              <ul className="main-menu space-y-5">
-                {["/", "/about", "/work", "/contact"].map((path, index) => (
-                  <li key={index} className={styles.navlink}>
-                    <Link
-                      ref={navLinkRef}
-                      href={path}
-                      className="text-4xl text-white uppercase  font-light cursor-pointer"
-                    >
-                      {path === "/" ? "Home" : path.substring(1).toUpperCase()}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-
-            <div className="header-nav-footer absolute bottom-5 left-1/2 transform -translate-x-1/2">
-              <ul className="social-links flex space-x-4">
-                {["Facebook", "Instagram", "Twitter"].map((social, index) => (
-                  <li key={index} className="text-white cursor-pointer">
-                    <Link href="#">{social}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-      </>
-    );
 
   return (
     <div className="flex justify-between items-center w-full absolute top-0 z-[5]">
